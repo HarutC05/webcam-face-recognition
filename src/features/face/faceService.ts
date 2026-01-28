1a5c453616c8b859010225101fcc1922e4a75998
HarutC05 <gasparharut@gmail.com>
2026-01-27 20:54:11 -0800

    Fix Netlify build: use browser-friendly face-api.js

--- src/features/face/faceService.ts
+++ src/features/face/faceService.ts
@@ -1,4 +1,4 @@
-import * as faceapi from "face-api.js";
+import * as faceapi from "face-api.js/dist/face-api.min.js";
 
 export const detectFaces = async (
     input: HTMLVideoElement | HTMLImageElement,
