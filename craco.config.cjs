6627ffa0257b35f004d08465c46ce0fdde026f72
HarutC05 <gasparharut@gmail.com>
2026-01-27 20:28:26 -0800

    Fix webpack polyfills for face-api.js

--- craco.config.cjs
+++ craco.config.cjs
@@ -1,11 +1,25 @@
+const webpack = require("webpack");
+
 module.exports = {
     webpack: {
         configure: (webpackConfig) => {
             webpackConfig.resolve = webpackConfig.resolve || {};
             webpackConfig.resolve.fallback = {
                 ...(webpackConfig.resolve.fallback || {}),
-                util: require.resolve("util/"),
+                crypto: require.resolve("crypto-browserify"),
+                stream: require.resolve("stream-browserify"),
+                buffer: require.resolve("buffer/"),
+                fs: false,
             };
+
+            webpackConfig.plugins = webpackConfig.plugins || [];
+            webpackConfig.plugins.push(
+                new webpack.ProvidePlugin({
+                    Buffer: ["buffer", "Buffer"],
+                    process: "process/browser",
+                }),
+            );
+
             return webpackConfig;
         },
     },
